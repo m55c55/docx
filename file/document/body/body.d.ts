3349192462bdb02c33d58961216d8625ee4ae321
Valentin Steinwandter <valentin.steinwandter@exputec.com>
2019-10-04 18:24:08 +0200

    remove all except build

---
+++ file/document/body/body.d.ts
@@ -0,0 +1,12 @@
+import { IXmlableObject, XmlComponent } from "../../../file/xml-components";
+import { SectionProperties, SectionPropertiesOptions } from "./section-properties/section-properties";
+export declare class Body extends XmlComponent {
+    private readonly defaultSection;
+    private readonly sections;
+    constructor(sectionPropertiesOptions?: SectionPropertiesOptions);
+    addSection(section: SectionPropertiesOptions | SectionProperties): void;
+    prepForXml(): IXmlableObject;
+    push(component: XmlComponent): void;
+    readonly DefaultSection: SectionProperties;
+    private createSectionParagraph;
+}
---
+++ file/media/data.d.ts
@@ -0,0 +1,18 @@
+export interface IMediaDataDimensions {
+    pixels: {
+        x: number;
+        y: number;
+    };
+    emus: {
+        x: number;
+        y: number;
+    };
+}
+export interface IMediaData {
+    referenceId: number;
+    stream: Buffer | Uint8Array | ArrayBuffer;
+    path?: string;
+    fileName: string;
+    dimensions: IMediaDataDimensions;
+}
+export declare const WORKAROUND2 = "";
